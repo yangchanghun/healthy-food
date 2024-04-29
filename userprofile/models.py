30526a036b2e426a55149b9bdf7e4e3b6c31986d
Gothax <scott990328@gmail.com>
2024-04-29 11:14:15 +0900

    new migration

--- userprofile/models.py
+++ userprofile/models.py
@@ -6,9 +6,8 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    user_image = models.ImageField(blank=True, null=True)
+    user_image = models.ImageField(blank=True, null=True, upload_to = 'user_images/')
     nickname = models.CharField(max_length=30)
-    email = models.EmailField()
     phone_number = models.CharField(max_length=15)
     address = models.CharField(max_length=100)
     detailed_address = models.CharField(max_length=100)
