a6266eda4d1634b4ede0fa6f2055c0e3ee46a51d
Gothax <scott990328@gmail.com>
2024-06-08 23:27:17 +0900

    버그수정

--- be/post/migrations/0002_product_seller.py
+++ be/post/migrations/0002_product_seller.py
@@ -16,7 +16,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='product',
             name='seller',
-            field=models.ForeignKey(default='91bc517d8607476782a8cc564818d536', on_delete=django.db.models.deletion.CASCADE, related_name='products', to=settings.AUTH_USER_MODEL),
+            field=models.ForeignKey(null=True, blank=True, on_delete=django.db.models.deletion.CASCADE, related_name='products', to=settings.AUTH_USER_MODEL),
             preserve_default=False,
         ),
     ]
